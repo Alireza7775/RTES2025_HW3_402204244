8f1510771718d6876a8fa72f6a56accaa0672124
Alireza7775 <110818256+Alireza7775@users.noreply.github.com>
2025-06-22 17:18:49 +0000

    Add files via upload

--- simple-web-server/simple-web-server.sh
+++ simple-web-server/simple-web-server.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-PORT=8080
+PORT=80
 
+while true; do
+  # Wait for an incoming connection and handle it
+  echo "[+] Received connection at $(date)"
+  echo -e "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\n$(date)" | nc -l -p $PORT -q 1
+done
 
